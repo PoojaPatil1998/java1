4ac81f2f6898dbaa521a79faa19020a14e1e42a4
PoojaPatil1998 <patil1998pooja@gmail.com>
2024-11-12 14:40:36 +0530

    Update Home.java

--- DemoGitProject/src/com/Home.java
+++ DemoGitProject/src/com/Home.java
@@ -6,6 +6,10 @@ public class Home
 	{
 		System.out.println("this is m1");
 	}
+	public void m2()
+	{
+		System.out.println("this is m2");
+	}
 public static void main(String[] args)
 {
 	System.out.println("this is git");
